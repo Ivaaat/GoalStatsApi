7730493d1700c4901409b77e95e2bb76a10afffd
ikonikhin <ivan.konikhin@stm-labs.ru>
2024-12-05 08:51:42 +0300

    Changes to be committed: 	modified:   app/config.py

--- app/config.py
+++ app/config.py
@@ -1,16 +1,18 @@
 import ssl
+from dotenv import load_dotenv
+import os
 
 
+load_dotenv()
+
+
+SSL_ROOT_CERTFILE  = os.getenv("SSL_ROOT_CERTFILE")
+SSL_CERTFILE = os.getenv("SSL_CERTFILE")
+SSL_KEYFILE = os.getenv("SSL_KEYFILE")
 
 
 def get_ssl_context() -> ssl.SSLContext:
-    # ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    # ssl_context.load_cert_chain(
-    #     certfile=os.getenv("SSL_CERTFILE", "path/to/fullchain.pem"),
-    #     keyfile=os.getenv("SSL_KEYFILE", "path/to/privkey.pem")
-    # )
     ssl_context = ssl._create_unverified_context()
-    # Если требуется клиентский сертификат и ключ, укажите их:
-    ssl_context.load_verify_locations(cafile=r'C:\postr\rootCA.pem') 
-    ssl_context.load_cert_chain(certfile=r'C:\postr\client.pem', keyfile=r'C:\postr\client.key')
+    ssl_context.load_verify_locations(cafile=SSL_ROOT_CERTFILE) 
+    ssl_context.load_cert_chain(certfile=SSL_CERTFILE, keyfile=SSL_KEYFILE)
     return ssl_context
